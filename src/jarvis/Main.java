6a65c5fe390f91934fe603aecb502bce98ae9a86
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-12-13 23:47:33 -0500

    Create main method

---
+++ src/jarvis/Main.java
@@ -0,0 +1,26 @@
+package jarvis;
+
+import httpServer.CommandArguments;
+import httpServer.HttpMethod;
+import httpServer.Server;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            runServer(args);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void runServer(String[] args) throws IOException {
+        Server server = new Server(new CommandArguments(args).port);
+        server.addRoute("/hello", HttpMethod.GET, new FileHandler("src/resources/hello.html"));
+        server.addRoute("/", HttpMethod.GET, new FileHandler("src/resources/index.html"));
+        server.addRoute("*", HttpMethod.GET, new NotFoundHandler("src/resources/notFound.html"));
+        server.listen();
+    }
+}
