87ed2ce950bd0e52737a973faa225f7425266f92
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-12-17 01:55:48 -0500

    refactor

--- src/jarvisTest/GuessingGameTest.java
+++ src/jarvisTest/GuessingGameTest.java
@@ -8,60 +8,66 @@ public class GuessingGameTest {
     @Test
     public void newGuessingGame() {
         GuessingGame game = new GuessingGame();
-        Assert.assertEquals(1, game.min);
-        Assert.assertEquals(100, game.max);
-        Assert.assertTrue(1 <= game.answer && game.answer <= 100);
+        testNewGame(game, 1, 100, 7);
         Assert.assertNotEquals(new GuessingGame().answer, game.answer);
+        Assert.assertEquals(1, new GuessingGame(0).guessLimit);
+        Assert.assertEquals(1, new GuessingGame(10, 20, 0).guessLimit);
+    }
 
+    @Test
+    public void newGameWithLimit() {
+        for (int limit : new int[] { 1, 2, 3, 4 }) {
+            GuessingGame game = new GuessingGame(limit);
+            testNewGame(game, 1, 100, limit);
+            Assert.assertNotEquals(game.answer, new GuessingGame(limit).answer);
+        }
+    }
+
+    @Test
+    public void newGameWithMinAndMax() {
+        GuessingGame game;
         int[][] initializers = {{10, 50}, {-1234, 50213}, {-300, -200}};
         for (int[] minMax : initializers) {
             int min = minMax[0];
             int max = minMax[1];
             game = new GuessingGame(min, max);
-            Assert.assertEquals(min, game.min);
-            Assert.assertEquals(7, game.guessLimit);
-            Assert.assertEquals(max, game.max);
-            Assert.assertTrue(min <= game.answer && game.answer <= max);
+            testNewGame(game, min, max, 7);
             Assert.assertNotEquals(new GuessingGame(min, max).answer, game.answer);
         }
+    }
 
-        initializers = new int[][] {{1, 1}, {1, 0}, {1, -1}};
+    @Test
+    public void newGameWithMinMaxAndLimit() {
+        int[][] initializers = {{10, 50, 4}, {3, 500, 7}};
+        for (int[] options : initializers) {
+            int min = options[0];
+            int max = options[1];
+            int limit = options[2];
+            GuessingGame game = new GuessingGame(min, max, limit);
+            testNewGame(game, min, max, limit);
+            Assert.assertNotEquals(game.answer, new GuessingGame(min, max, limit).answer);
+        }
+    }
+
+    @Test
+    public void newGameWithMinGreaterThanMax() {
+        int[][] initializers = new int[][] {{1, 1}, {1, 0}, {1, -1}};
         for (int[] minMax : initializers) {
             int min = minMax[0];
             int max = minMax[1];
-            game = new GuessingGame(min, max);
+            GuessingGame game = new GuessingGame(min, max);
             Assert.assertEquals(min, game.min);
             Assert.assertEquals(max, game.max);
             Assert.assertEquals(7, game.guessLimit);
             Assert.assertEquals(min, game.answer);
         }
+    }
 
-        for (int limit : new int[] { 1, 2, 3, 4 }) {
-            game = new GuessingGame(limit);
-            Assert.assertEquals(limit, game.guessLimit);
-            Assert.assertEquals(1, game.min);
-            Assert.assertEquals(100, game.max);
-            Assert.assertTrue(1 <= game.answer && game.answer <= 100);
-            Assert.assertNotEquals(game.answer, new GuessingGame(limit).answer);
-        }
-
-        game = new GuessingGame(0);
-        Assert.assertEquals(1, game.guessLimit);
-
-        for (int[] options : new int[][] {{10, 50, 4}, {3, 500, 7}}) {
-            int min = options[0];
-            int max = options[1];
-            int limit = options[2];
-            game = new GuessingGame(min, max, limit);
-            Assert.assertEquals(min, game.min);
-            Assert.assertEquals(max, game.max);
-            Assert.assertEquals(limit, game.guessLimit);
-            Assert.assertTrue(min <= game.answer && game.answer <= max);
-            Assert.assertNotEquals(game.answer, new GuessingGame(limit).answer);
-        }
-
-        game = new GuessingGame(10, 20, 0);
-        Assert.assertEquals(1, game.guessLimit);
+    private void testNewGame(GuessingGame game, int expectedMin, int expectedMax, int expectedLimit) {
+        Assert.assertEquals(expectedMin, game.min);
+        Assert.assertEquals(expectedMax, game.max);
+        Assert.assertEquals(expectedLimit, game.guessLimit);
+        Assert.assertTrue(expectedMin <= game.answer && game.answer <= expectedMax);
     }
 
     @Test
